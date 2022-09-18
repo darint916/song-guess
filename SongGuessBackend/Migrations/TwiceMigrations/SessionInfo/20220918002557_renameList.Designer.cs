be251f03e4a5e82df139dddd365fdecce8345607
Soulz <darint916@gmail.com>
2022-09-18 00:07:44 -0700

    migration(col rename)

--- SongGuessBackend/Migrations/TwiceMigrations/SessionInfo/20220918002557_renameList.Designer.cs
+++ SongGuessBackend/Migrations/TwiceMigrations/SessionInfo/20220918002557_renameList.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SongGuessBackend.Data.TwiceData;
@@ -12,9 +13,10 @@ using SongGuessBackend.Data.TwiceData;
 namespace SongGuessBackend.Migrations.TwiceMigrations.SessionInfo
 {
     [DbContext(typeof(TwiceSessionInfoContext))]
-    partial class TwiceSessionInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20220918002557_renameList")]
+    partial class renameList
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SongGuessBackend/Migrations/TwiceMigrations/SessionInfo/20220918002557_renameList.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SongGuessBackend.Migrations.TwiceMigrations.SessionInfo
+{
+    public partial class renameList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RandomSongIndex",
+                table: "SessionInfo",
+                newName: "RandomSongIndexList");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RandomSongIndexList",
+                table: "SessionInfo",
+                newName: "RandomSongIndex");
+        }
+    }
+}
