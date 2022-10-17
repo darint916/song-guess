6f0c68b7509f824a9571871d30751648c159370b
Soulz <darint916@gmail.com>
2022-10-17 16:16:45 -0700

    policy change

--- SongGuessBackend/Program.cs
+++ SongGuessBackend/Program.cs
@@ -54,7 +54,7 @@ builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization(options =>
 {
     // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
+    //options.FallbackPolicy = options.DefaultPolicy;
 });
 
 var app = builder.Build();
